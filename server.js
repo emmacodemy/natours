0da5cde6b9f2293f892eb5377b03d8460d6f6c46
emmacodemy <eayodeleof18@gmail.com>
2021-01-07 17:14:03 +0100

    updated package.json

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ mongoose
     console.log('DB connections successful');
   });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const server = app.listen(port, () => {
   console.log(`App is running on port ${port}...`);
